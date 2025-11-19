acd9451120f42596e04340286329b70e229b347c
Giuseppe <dicosola.job@gmail.com>
2025-11-19 10:19:07 +0100

    creata funzione di acquisizione dello sconosciuto ma non funziona

--- main.py
+++ main.py
@@ -3,81 +3,96 @@ import cv2 # pip install opencv-python
 
 webcam = cv2.VideoCapture(0)
 
-image_file = input("Target Image File > ")
-target_image = face_recognition.load_image_file(image_file)
-target_encoding = face_recognition.face_encodings(target_image)[0]
-
-print("Image Loaded. 128-dimension Face Encoding Generated. \n")
-
-target_name = input("Target Name > ")
-
-process_this_frame = True
-
-while True:
+def selfie_capture():
     ret, frame = webcam.read()
-
+    face_locations = face_recognition.face_locations(rgb_small_frame)
     small_frame = cv2.resize(frame, None, fx=0.20, fy=0.20)
     rgb_small_frame = cv2.cvtColor(small_frame, 4)
 
-    if process_this_frame:
+    label = "Sconosciuto"
+    ret, unknown_frame = webcam.read()
+    unknow_name=input("Inserisci il nome della persona sconosciuta: ")
+    cv2.imshow("Captured", unknown_frame)         
+    cv2.imwrite(unknow_name + ".png", unknown_frame)
+    process_this_frame = not process_this_frame
 
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        frame_encodings = face_recognition.face_encodings(rgb_small_frame)
+    if face_locations:
+        top, right, bottom, left = face_locations[0]
 
-        if frame_encodings:
-            frame_face_encoding = frame_encodings[0]
-            match = face_recognition.compare_faces([target_encoding], frame_face_encoding)[0]
-            if match:
-                label = target_name
-                process_this_frame = not process_this_frame
+        top *= 5
+        right *= 5
+        bottom *= 5
+        left *= 5
 
-                if face_locations:
-                    top, right, bottom, left = face_locations[0]
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 0, 255), cv2.FILLED)
+        label_font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, label, (left + 6, bottom - 6), label_font, 0.8, (255, 255, 255), 1)
 
-                    top *= 5
-                    right *= 5
-                    bottom *= 5
-                    left *= 5
+        cv2.imshow("Persona Sconosciuta", unknown_frame)
+    return
 
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
-                    cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 255, 0), cv2.FILLED)
-                    label_font = cv2.FONT_HERSHEY_DUPLEX
-                    cv2.putText(frame, label, (left + 6, bottom - 6), label_font, 0.8, (255, 255, 255), 1)
-                    cv2.imshow("Video Feed", frame)
 
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-            
-            else:
-                label = "Sconosciuto"
-                ret, unknown_frame = webcam.read()
-                unknow_name=input("Inserisci il nome della persona sconosciuta: ")
-                cv2.imshow("Captured", unknown_frame)         
-                cv2.imwrite(unknow_name + ".png", unknown_frame)
-                process_this_frame = not process_this_frame
 
-                if face_locations:
-                    top, right, bottom, left = face_locations[0]
-
-                    top *= 5
-                    right *= 5
-                    bottom *= 5
-                    left *= 5
-
-                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                    cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 0, 255), cv2.FILLED)
-                    label_font = cv2.FONT_HERSHEY_DUPLEX
-                    cv2.putText(frame, label, (left + 6, bottom - 6), label_font, 0.8, (255, 255, 255), 1)
-
-                    cv2.imshow("Video Feed", frame)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+def main():
     
+    image_file = input("Target Image File > ")
+    target_image = face_recognition.load_image_file(image_file)
+    target_encoding = face_recognition.face_encodings(target_image)[0]
 
-    
+    print("Image Loaded. 128-dimension Face Encoding Generated. \n")
 
-    
+    target_name = input("Target Name > ")
 
-webcam.release()
-cv2.destroyAllWindows()
+    process_this_frame = True
+
+    while True:
+        ret, frame = webcam.read()
+
+        small_frame = cv2.resize(frame, None, fx=0.20, fy=0.20)
+        rgb_small_frame = cv2.cvtColor(small_frame, 4)
+
+        if process_this_frame:
+
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            frame_encodings = face_recognition.face_encodings(rgb_small_frame)
+
+            if frame_encodings:
+                frame_face_encoding = frame_encodings[0]
+                match = face_recognition.compare_faces([target_encoding], frame_face_encoding)[0]
+                if match:
+                    label = target_name
+                    process_this_frame = not process_this_frame
+
+                    if face_locations:
+                        top, right, bottom, left = face_locations[0]
+
+                        top *= 5
+                        right *= 5
+                        bottom *= 5
+                        left *= 5
+
+                        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+                        cv2.rectangle(frame, (left, bottom - 30), (right, bottom), (0, 255, 0), cv2.FILLED)
+                        label_font = cv2.FONT_HERSHEY_DUPLEX
+                        cv2.putText(frame, label, (left + 6, bottom - 6), label_font, 0.8, (255, 255, 255), 1)
+                        cv2.imshow("Video Feed", frame)
+
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
+                
+                else:
+                    selfie_capture()
+        
+
+        
+
+        
+
+    webcam.release()
+    cv2.destroyAllWindows()
+
+    if __name__ == "__main__":
+        main()
