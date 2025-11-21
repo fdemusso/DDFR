a716539841afe47d131d14c4680a11b36c611245
Flavio De Musso <Demusso1617@gmail.com>
2025-11-21 13:27:23 +0100

    Aggiunti controlli per non ripetere codice

--- src/recognition.py
+++ src/recognition.py
@@ -14,8 +14,32 @@ IMG_FOLDER = "Img"
 TEMP_FOLDER = "Temp"
 logger = logging.getLogger(__name__)
 
+def GetAllFaceEncodings():
+    # Leggo il database esistente o creo dict vuoto
+    if os.path.exists(DATABASE_PATH) and os.path.getsize(DATABASE_PATH) > 0:
+        with open(DATABASE_PATH, 'rb') as f:
+            all_face_encodings = pickle.load(f)
+    else:
+        all_face_encodings = {}
+    return all_face_encodings
+
+def IsNameInDatabase(name):
+
+    all_face_encodings = GetAllFaceEncodings()
+
+    face_names = list(all_face_encodings.keys())
+    if name in face_names:
+        logger.info(f"Volto di {name} già presente")
+        return True
+    else:
+        return False
+
 def ConvertAnyToPng(FilePath, name, ext):
    
+    if IsNameInDatabase(name):
+        logger.debug(f"Volto di {name} già presente, salto la conversione.")
+        return None
+    
     filepng = f"{IMG_FOLDER}/{TEMP_FOLDER}/{name}.png"
 
     if os.path.exists(filepng):
@@ -58,24 +82,14 @@ def RecognitionFromFile(FilePath, name):
     # Verifico che il file esista
     if FilePath is None:
         return False
-    
-    # Leggo il database esistente o creo dict vuoto
-    if os.path.exists(DATABASE_PATH) and os.path.getsize(DATABASE_PATH) > 0:
-        with open(DATABASE_PATH, 'rb') as f:
-            all_face_encodings = pickle.load(f)
-    else:
-        all_face_encodings = {}
+    if not IsNameInDatabase(name):
 
-    face_names = list(all_face_encodings.keys())
-
-    # faccio l'encoding solo per i nomi che non sono presenti
-    if name in face_names:
-        logger.info(f"Volto di {name} già presente")
-    else:
+        all_face_encodings = GetAllFaceEncodings()
         File_target = face_recognition.load_image_file(FilePath)
         all_face_encodings[name] = face_recognition.face_encodings(File_target, num_jitters=100)[0]
         logger.debug(f"{name}: Image Loaded. 128-dimension Face Encoding Generated.\n")
-
+    else:
+        return True
     # Risalvo tutto il dict
     with open(DATABASE_PATH, 'wb') as f:
         pickle.dump(all_face_encodings, f)
@@ -103,12 +117,15 @@ def FolderScan():
         # Prendo tutti gli elementi di IMG_FOLDER
         full_path = os.path.join(IMG_FOLDER, filename)
         name, ext = os.path.splitext(filename)
+        
+        if IsNameInDatabase(name):
+            continue
+
         logger.debug(f"Trovato file: {full_path},{filename} con estensione: {ext}")
         PngPath = ConvertAnyToPng(full_path, name, ext)
 
         # Filtro per quelli che è possibile convertire
         if PngPath is None:
-            logger.error(f"Impossibile convertire il file: {filename}")
             continue
         else:
             if (RecognitionFromFile(PngPath, name)):
